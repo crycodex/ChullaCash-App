9fe41c6889feffa65fb90457a72b2acec1553720
Cristhian David Recalde <crecalde@neaternotes.com>
2025-01-15 20:07:49 -0500

    add get x

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'package:get/get.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -15,7 +15,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return GetMaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
